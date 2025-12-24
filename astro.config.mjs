0d0063d11bb7b524877650023525ed9768a6b923
Ashik <ashik.salih@gmail.com>
2025-12-24 17:53:53 +0000

    Changed path to run dynamically

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,6 +11,6 @@ import tailwind from "@astrojs/tailwind";
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://ashik-ms.github.io',
+  site: 'https://ashik-ms.github.io/astro-portfolio',
   base: '/astro-portfolio',
 });
